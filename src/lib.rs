621a93e1af11b3772a7e38d5320f58b5671d9754
Nick Bozarth <nickbozarth2023@gmail.com>
2026-08-27 23:50:58 -0500

    build: set feature exclusivity errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
 #![no_std]
 
 pub mod embedded_utils;
+
+#[cfg(all(feature = "ram-16k", feature = "ram-64k"))]
+compile_error!("Features `ram-16k` and `ram-64k` cannot be built together");
+
+#[cfg(not(any(feature = "ram-16k", feature = "ram-64k")))]
+compile_error!("Select ONE memory target feature: --features [`ram-16k`, `ram-64k`]");
